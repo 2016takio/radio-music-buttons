5cc7e2b585493340414cc234cf2171c0ae5722a6
2016takio <2016takio@gmail.com>
2017-09-10 22:19:29 +0900

    複数ボタンでそれぞれのBGMが再生できるように機能追加

--- soundplay.js
+++ soundplay.js
@@ -1,5 +1,20 @@
-function sound()
+function sound(element)
 {
+
+	//ファイルのフルパスを取得
+	var myPath = location.pathname;
+	//フォルダパスを取得
+	var dir_path = myPath.split("/").reverse().slice(1).reverse().join("/");
+
+	// 生成するエレメントの調整
+	var audio = document.createElement( 'audio' ) ;
+	// ID要素を設定
+	audio.id = 'sound_' + element.id ;
+	// src要素を設定
+	audio.src = dir_path + '/music/' + element.id + '/1.mp3' ;
+	// body要素の一番後ろに[audio]を生成する
+	document.body.appendChild( audio ) ;
+
 	// [ID:sound-file]の音声ファイルを再生[play()]する
-	document.getElementById( 'sound-file' ).play() ;
+	document.getElementById( audio.id ).play() ;
 }
