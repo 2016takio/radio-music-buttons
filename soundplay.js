e63bcc593f7b9145b8b85dea71984e9d1872dcb4
2016takio <2016takio@gmail.com>
2017-11-08 23:32:01 +0900

    連続再生に対応してオマケでボタンを増やしたりした

--- soundplay.js
+++ soundplay.js
@@ -11,10 +11,15 @@ function sound(element)
 	// ID要素を設定
 	audio.id = 'sound_' + element.id ;
 	// src要素を設定
-	audio.src = dir_path + '/music/' + element.id + '/1.mp3' ;
+	audio.src = dir_path + '/music/' + element.id + '/1.wav' ;
 	// body要素の一番後ろに[audio]を生成する
-	document.body.appendChild( audio ) ;
+	document.body.appendChild( audio );
 
+	// [currentTime]を認識した場合のみ、処理をする
+	if( typeof( document.getElementById( audio.id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( audio.id ).currentTime = 0;
+	}
 	// [ID:sound-file]の音声ファイルを再生[play()]する
 	document.getElementById( audio.id ).play() ;
 }
